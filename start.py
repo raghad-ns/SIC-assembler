22abf1815a88b28039d8d1fd448222b0b8fc0456
raghad-ns <raghadnadersalem20022017@gmail.com>
2024-03-23 13:12:26 +0200

    fixes

--- start.py
+++ start.py
@@ -40,37 +40,42 @@ def subHex (a, b):
 def pass1() :
    source = open("source.asm", 'r')
    intermediate = open("intermediate.mdt", 'w')
-   errors = open('errors.tsx', "w")
+   errors = open('errors.txt', "w")
    pass1Out = open("pass1-out", 'w')
    sourceCode = source.read()
    lines = sourceCode.split('\n')
    symbolTable = {}
-   locationCounter = "0000"
+   locationCounter = "0000" # Initialize location counter to 0
    programLength = 0
    programName = ""
    startingAddress = ""
+
    if lines[0][11:21].strip() == "START": # handle the start of program label
       startingAddress = lines[0][21:30].strip()
-      locationCounter = startingAddress
+      locationCounter = startingAddress # set location counter to the address given in START directive
       intermediate.write(locationCounter + "     "+ lines[0][:30]+ '\n')
       programName = lines[0][21:30].strip()
+
+   #  Process each line of code
    for instruction in lines[1:] :
       label = instruction[0:11]
       opcode = instruction[11:21].strip()
       operand = instruction[21:30]
-      if label.strip() == ".": # its a comment line
-         continue
+
+      if instruction[0] == ".": # its a comment line
+         continue # skip
+
       if label.strip() != "": # if the line contains label
          if label in symbolTable: # check if it is already defined
-            print("Duplicated Label Error")
+            print("Duplicated Label Error") # Set error flag
             errors.write("Error: Duplicated Label Error \n")
             errors.write("\t"+ instruction)
             break
          else:
             symbolTable[label] = locationCounter # insert into the symbol table
+
       if opcode in opcodeTable: # valid opcode found
-         tempLocationCounter = sumHex(locationCounter, "3")
-         intermediate.write(locationCounter + "     "+ instruction[:30]+ '\n')
+         tempLocationCounter = sumHex(locationCounter, "3") # Updata location  counter by adding 3 (size of current instruction)
        #   print("location counter: ", locationCounter)
       else :
          if opcode == "WORD": 
@@ -89,6 +94,7 @@ def pass1() :
             errors.write("Invalid Opcode Error", opcode)
             errors.write("\t"+ instruction)
             break
+      intermediate.write(locationCounter + "     "+ instruction[:30]+ '\n')
       locationCounter = tempLocationCounter
       if opcode == "END": # end of program reached
          programLength = subHex(locationCounter, startingAddress)
